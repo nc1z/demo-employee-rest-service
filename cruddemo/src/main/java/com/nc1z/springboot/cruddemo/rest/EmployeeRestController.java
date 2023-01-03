e3bc3a54998d3e3889ac9b9b177d8939e1fc8a56
nc1z <nc1zdev@gmail.com>
2023-01-03 12:17:09 +0800

    [rest] updated all CRUD methods

--- cruddemo/src/main/java/com/nc1z/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/nc1z/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.nc1z.springboot.cruddemo.dao.EmployeeDAO;
 import com.nc1z.springboot.cruddemo.entity.Employee;
 import com.nc1z.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,52 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+        // To force a save of new item with saveOrUpdate (deprecated), set id to 0
+        // TODO: Update new method once saveOrUpdate replaced
+
+        theEmployee.setId(0);
+
+        employeeService.save(theEmployee);
+
+        return theEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+
+        employeeService.save(theEmployee);
+
+        return theEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
     }
 
 }
