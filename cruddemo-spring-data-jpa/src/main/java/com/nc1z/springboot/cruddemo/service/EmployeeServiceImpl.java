ee6d742bccc7dcc23ba3003dac2c6df408b1e7c3
nc1z <nc1zdev@gmail.com>
2023-01-03 13:45:00 +0800

    [EmployeeServiceImpl] refactor service impl to use EmployeeRepository & JpaRepository

--- cruddemo-spring-data-jpa/src/main/java/com/nc1z/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ cruddemo-spring-data-jpa/src/main/java/com/nc1z/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,41 +1,46 @@
 package com.nc1z.springboot.cruddemo.service;
 
+import com.nc1z.springboot.cruddemo.dao.EmployeeRepository;
 import com.nc1z.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
-    @Qualifier("employeeDAOJPAImpl")
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
 
     @Override
-    @Transactional
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
-    @Transactional
     public Employee findById(int theId) {
-        return employeeDAO.findById(theId);
+        Optional<Employee> result = employeeRepository.findById(theId);
+
+        Employee theEmployee = null;
+        if (result.isPresent()) {
+            theEmployee = result.get();
+        } else {
+            throw new RuntimeException("Did not find employee id - " + theId);
+        }
+
+        return theEmployee;
     }
 
     @Override
-    @Transactional
     public void save(Employee theEmployee) {
-        employeeDAO.save(theEmployee);
+        employeeRepository.save(theEmployee);
     }
 
     @Override
-    @Transactional
     public void deleteById(int theId) {
-        employeeDAO.deleteById(theId);
+        employeeRepository.deleteById(theId);
     }
 }
